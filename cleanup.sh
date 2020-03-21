dd3ca828e55a6f968a7720e5000b5857967a09f9
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-03-21 18:29:28 -0500

    More helper script

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+rm -rfv dist build
+rm -rfv PyGMLParser.egg-info
+rm -rfv html
+
+echo 'CLEANED UP'
---
+++ gendoc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+rm -rf html
+
+pdoc3 --force --html org/
+
